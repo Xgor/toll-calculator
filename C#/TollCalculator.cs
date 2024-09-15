a8192fae93014b473d690693824a4c0260693d01
Xgor <real.xgor@hotmail.com>
2024-09-15 12:35:31 +0200

    Test Enviroment and Notes setup

--- C#/TollCalculator.cs
+++ C#/TollCalculator.cs
@@ -2,107 +2,180 @@
 using System.Globalization;
 using TollFeeCalculator;
 
-public class TollCalculator
+namespace TollFeeCalculator
 {
-
-    /**
-     * Calculate the total toll fee for one day
-     *
-     * @param vehicle - the vehicle
-     * @param dates   - date and time of all passes on one day
-     * @return - the total toll fee for that day
-     */
-
-    public int GetTollFee(Vehicle vehicle, DateTime[] dates)
+    public class TollCalculator
     {
-        DateTime intervalStart = dates[0];
-        int totalFee = 0;
-        foreach (DateTime date in dates)
+
+        /**
+         * Calculate the total toll fee for one day
+         *
+         * @param vehicle - the vehicle
+         * @param dates   - date and time of all passes on one day
+         * @return - the total toll fee for that day
+         */
+
+        public int GetTollFee(Vehicle vehicle, DateTime[] dates)
         {
-            int nextFee = GetTollFee(date, vehicle);
-            int tempFee = GetTollFee(intervalStart, vehicle);
+            // Return zero if array is empty
+            if (dates == null || dates.Length == 0) return 0;
 
-            long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-            long minutes = diffInMillies/1000/60;
+            //
 
-            if (minutes <= 60)
+            // Om jag ska behålla intervalStart så behövs sortera dates i tidsordning. Man hoppas det är i tidsordning men man vet aldrig
+            DateTime intervalStart = dates[0];
+            int totalFee = 0;
+            foreach (DateTime date in dates)
             {
-                if (totalFee > 0) totalFee -= tempFee;
-                if (nextFee >= tempFee) tempFee = nextFee;
-                totalFee += tempFee;
+                int nextFee = GetTollFee(date, vehicle);
+                int tempFee = GetTollFee(intervalStart, vehicle); // Funderar man kan ha denna utanför loopen så man inte behöver hämta fees två gånger per loop
+
+                long diffInMillies = date.Millisecond - intervalStart.Millisecond;
+                long minutes = diffInMillies / 1000 / 60; // Varför inte bara göra date.minutes ?
+
+                // Denna kodsnutten är mest förvirrande och tror inte den ens funkar. 
+                // Tror den är för att kolla så det inte blir för flera gånger men den kollar bara mot ett datum och kollar om det är större inte mindre
+                // Tror idéen är att uppdatera interval start så kollar med den närmaste men det händer inte just nu.
+                if (minutes <= 60)
+                {
+                    if (totalFee > 0) totalFee -= tempFee;
+                    if (nextFee >= tempFee) tempFee = nextFee;
+                    totalFee += tempFee;
+                }
+                else
+                {
+                    totalFee += nextFee;
+                }
             }
-            else
-            {
-                totalFee += nextFee;
-            }
+            if (totalFee > 60) totalFee = 60; // Felplacerad. Detta gör så att det bara går att max få 60 kr på fakturan Behövs nog en loop för varje dag.
+            return totalFee;
         }
-        if (totalFee > 60) totalFee = 60;
-        return totalFee;
-    }
 
-    private bool IsTollFreeVehicle(Vehicle vehicle)
-    {
-        if (vehicle == null) return false;
-        String vehicleType = vehicle.GetVehicleType();
-        return vehicleType.Equals(TollFreeVehicles.Motorbike.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Tractor.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Emergency.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Diplomat.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Foreign.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Military.ToString());
-    }
-
-    public int GetTollFee(DateTime date, Vehicle vehicle)
-    {
-        if (IsTollFreeDate(date) || IsTollFreeVehicle(vehicle)) return 0;
-
-        int hour = date.Hour;
-        int minute = date.Minute;
-
-        if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-        else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-        else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-        else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-        else return 0;
-    }
-
-    private Boolean IsTollFreeDate(DateTime date)
-    {
-        int year = date.Year;
-        int month = date.Month;
-        int day = date.Day;
-
-        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
-
-        if (year == 2013)
+        private bool IsTollFreeVehicle(Vehicle vehicle) // TODO: Skapa Tractor, Emergency,Diplomat ,Foreign,Military
         {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
+            if (vehicle == null) return false;
+            String vehicleType = vehicle.GetVehicleType();
+            return vehicleType.Equals(TollFreeVehicles.Motorbike.ToString()) ||
+                   vehicleType.Equals(TollFreeVehicles.Tractor.ToString()) ||
+                   vehicleType.Equals(TollFreeVehicles.Emergency.ToString()) ||
+                   vehicleType.Equals(TollFreeVehicles.Diplomat.ToString()) ||
+                   vehicleType.Equals(TollFreeVehicles.Foreign.ToString()) ||
+                   vehicleType.Equals(TollFreeVehicles.Military.ToString());
+            // Denna return är super ful, kan göra den mer elegant Kommer inte fungera nog för den checkar string med int
         }
-        return false;
-    }
 
-    private enum TollFreeVehicles
-    {
-        Motorbike = 0,
-        Tractor = 1,
-        Emergency = 2,
-        Diplomat = 3,
-        Foreign = 4,
-        Military = 5
+        public int GetTollFee(DateTime date, Vehicle vehicle)
+        {
+            if (IsTollFreeDate(date) || IsTollFreeVehicle(vehicle)) return 0;
+
+            int hour = date.Hour;
+            int minute = date.Minute;
+
+            // Känns som man skulle kunna göra denna lite mer läsbar. Finn definift delar som inte behövs
+            if (hour == 6 && minute >= 0 && minute <= 29) return 8;
+            else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
+            else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
+            else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
+            else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
+            else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
+            else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
+            else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
+            else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+            else return 0;
+        }
+
+        private Boolean IsTollFreeDate(DateTime date)
+        {
+            int year = date.Year;
+            int month = date.Month;
+            int day = date.Day;
+
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
+
+            if (year == 2013) // Detta behöver modiferas för att kunna hantera högtider. Specifikt Midsommar, Kristi himmelsfärd och Påsk behöver jag kolla in.
+            {
+                if (month == 1 && (day == 1 || day == 6) || // La till Trettondag
+                    month == 3 && (day == 28 || day == 29) || // Modifera för påsk med DateUtils.EasterSunday
+                    month == 4 && (day == 1 || day == 30) || // Första april är inte högtid... inte heller valborg
+                    month == 5 && (day == 1 || day == 8 || day == 9) ||
+                    month == 6 && (day == 5 || day == 6 || day == 21) || // 5 juni högtid?
+                    month == 7 || // Är hela juli en högtid?
+                    month == 11 && day == 1 || // Behövs Alla helgons dag hanteras? Är på en lördag
+                    month == 12 && (day == 24 || day == 25 || day == 26 || day == 31)) // Räknas julafton och nyårsdagen?
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private enum TollFreeVehicles
+        {
+            Motorbike = 0,
+            Tractor = 1,
+            Emergency = 2,
+            Diplomat = 3,
+            Foreign = 4,
+            Military = 5
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine(args.Length);
+        }
     }
 }
+
+/*
+public class TollTester
+{
+    public void runUnitTest()
+    {
+        TollCalculator toll = new TollCalculator();
+        Car testCar = new Car();
+        DateTime[] testChristmas = new DateTime[new DateTime(2013, 12, 25, 10, 30, 50),new DateTime(2013, 12, 25, 10, 50, 50)]
+        if 0 ==toll.GetTollFee(testCar,testChristmas):
+            Console.Write("testChristmas SUCCESS");
+        else:
+            Console.Write("testChristmas FAIL");
+        DateTime[] testNormal = new DateTime[new DateTime(2013, 12, 22, 10, 30, 50),new DateTime(2013, 12, 22, 10, 50, 50)]
+        
+        if 0 ==toll.GetTollFee(testCar,testNormal):
+            Console.Write("testChristmas SUCCESS");
+        else:
+            Console.Write("testChristmas FAIL");
+    }
+}
+*/
+
+
+// Från
+// https://web.archive.org/web/20120223154950/https://aa.usno.navy.mil/faq/docs/easter.php
+//  https://codereview.stackexchange.com/questions/193847/find-easter-on-any-given-year
+// Gauss Easter algorithm
+
+/*
+public static DateTime EasterSunday(int year)
+{
+    int day = 0;
+    int month = 0;
+
+    int g = year % 19;
+    int c = year / 100;
+    int h = (c - (int)(c / 4) - (int)((8 * c + 13) / 25) + 19 * g + 15) % 30;
+    int i = h - (int)(h / 28) * (1 - (int)(h / 28) * (int)(29 / (h + 1)) * (int)((21 - g) / 11));
+
+    day   = i - ((year + (int)(year / 4) + i + 2 - c + (int)(c / 4)) % 7) + 28;
+    month = 3;
+
+    if (day > 31)
+    {
+        month++;
+        day -= 31;
+    }
+
+    return new DateTime(year, month, day);
+}
+*/
---
+++ TollCalculatorTest/TollCalculatorTests.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TollFeeCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System.Runtime.ConstrainedExecution;
+
+namespace TollFeeCalculator.Tests
+{
+    [TestClass()]
+    public class TollCalculatorTests
+    {
+        private readonly TollCalculator _tollCalculator;
+        private readonly Vehicle car;
+        private readonly Vehicle motorbike;
+        private readonly DateTime[] emptyDateTimes;
+        private readonly DateTime[] rushHourDayTimes;
+        private readonly DateTime[] shuffledRushHourDayTimes;
+        private readonly DateTime[] easter2024Times;
+        private readonly DateTime[] easter2025Times;
+        private readonly DateTime[] oneWeekNoonTimes;
+        private readonly DateTime[] twoFilledDaysTimes;
+        private readonly DateTime[] holidays2024DateTimes;
+        public TollCalculatorTests()
+        {
+            _tollCalculator = new TollCalculator();
+            car = new Car();
+            motorbike = new Motorbike();
+            emptyDateTimes = Array.Empty<DateTime>();
+            rushHourDayTimes = new DateTime[] { 
+                new DateTime(2024, 09, 16, 07, 00, 00),
+                new DateTime(2024, 09, 16, 07, 30, 00),
+                new DateTime(2024, 09, 16, 16, 00, 00),
+                new DateTime(2024, 09, 16, 16, 30, 00),
+            };
+            shuffledRushHourDayTimes = new DateTime[] {
+                new DateTime(2024, 09, 16, 16, 30, 00),
+                new DateTime(2024, 09, 16, 16, 00, 00),
+                new DateTime(2024, 09, 16, 07, 30, 00),
+                new DateTime(2024, 09, 16, 07, 00, 00),
+
+            };
+            easter2024Times = new DateTime[] {
+                new DateTime(2024, 03, 29, 07, 00, 00),
+                new DateTime(2024, 03, 30, 07, 30, 00),
+                new DateTime(2024, 03, 31, 16, 00, 00),
+                new DateTime(2024, 04, 01, 16, 30, 00),
+            };
+            easter2025Times = new DateTime[] {
+                new DateTime(2025, 04, 18, 07, 00, 00),
+                new DateTime(2025, 04, 19, 07, 30, 00),
+                new DateTime(2025, 04, 20, 16, 00, 00),
+                new DateTime(2025, 04, 21, 16, 30, 00),
+            };
+            oneWeekNoonTimes = new DateTime[] {
+                new DateTime(2024, 09, 16, 12, 00, 00),
+                new DateTime(2024, 09, 17, 12, 30, 00),
+                new DateTime(2024, 09, 18, 12, 00, 00),
+                new DateTime(2024, 09, 19, 12, 30, 00),
+                new DateTime(2024, 09, 20, 12, 30, 00),
+                new DateTime(2024, 09, 21, 12, 00, 00),
+                new DateTime(2024, 09, 22, 12, 30, 00),
+            };
+
+            
+            twoFilledDaysTimes = new DateTime[] {
+                new DateTime(2024, 09, 16, 6, 00, 00),
+                new DateTime(2024, 09, 16, 7, 00, 00),
+                new DateTime(2024, 09, 16, 8, 00, 00),
+                new DateTime(2024, 09, 16, 9, 00, 00),
+                new DateTime(2024, 09, 16, 10, 00, 00),
+                new DateTime(2024, 09, 16, 11, 00, 00),
+                new DateTime(2024, 09, 16, 12, 00, 00),
+                new DateTime(2024, 09, 16, 13, 00, 00),
+                new DateTime(2024, 09, 16, 14, 00, 00),
+                new DateTime(2024, 09, 16, 15, 00, 00),
+                new DateTime(2024, 09, 16, 16, 00, 00),
+                new DateTime(2024, 09, 16, 17, 00, 00),
+                new DateTime(2024, 09, 16, 18, 00, 00),
+                new DateTime(2024, 09, 16, 19, 00, 00),
+                new DateTime(2024, 09, 17, 6, 00, 00),
+                new DateTime(2024, 09, 17, 7, 00, 00),
+                new DateTime(2024, 09, 17, 8, 00, 00),
+                new DateTime(2024, 09, 17, 9, 00, 00),
+                new DateTime(2024, 09, 17, 10, 00, 00),
+                new DateTime(2024, 09, 17, 11, 00, 00),
+                new DateTime(2024, 09, 17, 12, 00, 00),
+                new DateTime(2024, 09, 17, 13, 00, 00),
+                new DateTime(2024, 09, 17, 14, 00, 00),
+                new DateTime(2024, 09, 17, 15, 00, 00),
+                new DateTime(2024, 09, 17, 16, 00, 00),
+                new DateTime(2024, 09, 17, 17, 00, 00),
+                new DateTime(2024, 09, 17, 18, 00, 00),
+                new DateTime(2024, 09, 17, 19, 00, 00),
+            };
+            holidays2024DateTimes = new DateTime[] {
+                new DateTime(2024, 01, 01, 16, 30, 00),
+                new DateTime(2024, 01, 06, 16, 30, 00),
+                new DateTime(2024, 03, 29, 07, 00, 00),
+                new DateTime(2024, 03, 30, 07, 30, 00),
+                new DateTime(2024, 03, 31, 16, 00, 00),
+                new DateTime(2024, 04, 01, 16, 30, 00),
+                new DateTime(2024, 05, 01, 16, 30, 00),
+                new DateTime(2024, 05, 09, 16, 30, 00),
+                new DateTime(2024, 05, 19, 16, 30, 00),
+                new DateTime(2024, 06, 06, 16, 30, 00),
+                new DateTime(2024, 12, 25, 16, 30, 00),
+                new DateTime(2024, 12, 26, 16, 30, 00),
+            };
+        }
+        [TestMethod()]
+        public void TollFeeEmptyTest()
+        {
+            int value = _tollCalculator.GetTollFee(car, emptyDateTimes);
+
+            Assert.AreEqual(0, value);
+        }
+        [TestMethod()]
+        public void TollFeeNullTest()
+        {
+            int value = _tollCalculator.GetTollFee(null, null);
+
+            Assert.AreEqual(0, value);
+        }
+
+        [TestMethod()]
+        public void TollMotorbikeTest()
+        {
+            int value = _tollCalculator.GetTollFee(motorbike, rushHourDayTimes);
+
+            Assert.AreEqual(0, value);
+        }
+
+        [TestMethod()]
+        public void TollRushHourTest()
+        {
+            int value = _tollCalculator.GetTollFee(car, rushHourDayTimes);
+
+            Assert.AreEqual(36, value);
+        }
+        [TestMethod()]
+        public void TollShuffledRushHourTest()
+        {
+            int value = _tollCalculator.GetTollFee(car, shuffledRushHourDayTimes);
+
+            Assert.AreEqual(36, value);
+        }
+
+        
+        [TestMethod()]
+        public void TollEaster2024Test()
+        {
+            int value = _tollCalculator.GetTollFee(car, easter2024Times);
+
+            Assert.AreEqual(36, value);
+        }
+
+        [TestMethod()]
+        public void TollEaster2025Test()
+        {
+            int value = _tollCalculator.GetTollFee(car, easter2025Times);
+
+            Assert.AreEqual(36, value);
+        }
+
+        [TestMethod()]
+        public void TollOneWeekNoonTest()
+        {
+            int value = _tollCalculator.GetTollFee(car, oneWeekNoonTimes);
+
+            Assert.AreEqual(40, value);
+        }
+
+        [TestMethod()]
+        public void TollTwoFilledDaysTest()
+        {
+            int value = _tollCalculator.GetTollFee(car, oneWeekNoonTimes);
+
+            Assert.AreEqual(120, value);
+        }
+        [TestMethod()]
+        public void TollHolidays2024Test()
+        {
+            int value = _tollCalculator.GetTollFee(car, holidays2024DateTimes);
+
+            Assert.AreEqual(0, value);
+        }
+        
+
+    }
+}
